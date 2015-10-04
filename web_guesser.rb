cb9a8e209aa6c0a95d0bdfc55b02344457cd2963
Ross Edfort <rossedfort@gmail.com>
2015-10-04 15:34:51 -0600

    color changer extension working

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,55 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def secret_number
+  SECRET_NUMBER
+end
+
+def check_guess(guess)
+  guess = guess.to_i unless guess.nil?
+  too_high = secret_number + 5
+  too_low = secret_number - 5
+  if guess.nil?
+    @color = "#FFFFFF"
+    "Guess!"
+  elsif guess > too_high
+    @color = "#FF3300"
+    "Wayyyy Too high!"
+  elsif guess < too_low
+    @color = "#FF3300"
+    "Wayyyy Too low!"
+  elsif guess < secret_number
+    @color = "#FF6600"
+    "Too Low!"
+  elsif guess > secret_number
+    @color = "#FF6600"
+    "Too High!"
+  else
+    @color = "#00FF00"
+    "Got it! -- The Secret Number is #{secret_number}"
+  end
+end
+
+# def color_changer
+#   if @@message == "Guess!"
+#     "background-color:#FFFFFF"
+#   elsif @@message == "Wayyyy Too high!" || "Wayyyy Too low!"
+#     "background-color:#FF3300"
+#   elsif @@message == "Too High!" || "Too Low!"
+#     "background-color:#FF6600"
+#   elsif @@message == "Got it! -- The Secret Number is #{secret_number}"
+#     "background-color:#00FF00"
+#   end
+# end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  @@message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER,
+                          :message => @@message,
+                          :color => @color
+                        }
 end
